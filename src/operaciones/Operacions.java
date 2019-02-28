cf05eb0f2925645e01933e504fdef71b43fb2de3
Sergio Gomez <sergiosanvi3@gmail.com>
2019-02-28 19:19:22 +0100

    Operacions1

---
+++ src/operaciones/Operacions.java
@@ -0,0 +1,18 @@
+package operaciones;
+public class Operacions {
+
+	public double getIterativa(int n) {
+		if (n < 0) {
+			throw new IllegalArgumentException();
+		}
+		if (n == 1 || n == 0) {
+			return 1;
+		}
+
+		int aux = 1;
+		for (int i = 2; i <= n; i++) {
+			aux *= i;
+		}
+		return aux;
+	}
+}
