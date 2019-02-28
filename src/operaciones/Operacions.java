2623b5905a3e15470d985d6499df0472cbd6cdb7
Sergio Gomez <sergiosanvi3@gmail.com>
2019-02-28 19:21:17 +0100

    Modificacio Recursiva

--- src/operaciones/Operacions.java
+++ src/operaciones/Operacions.java
@@ -1,5 +1,17 @@
 package operaciones;
+
 public class Operacions {
+
+	public double getRecursiva(int n) {
+		if (n < 0) {
+			throw new IllegalArgumentException();
+		}
+		if (n == 1 || n == 0) {
+			return 1;
+		}
+		Operacions fmenos1 = new Operacions();
+		return n * fmenos1.getRecursiva(n - 1);
+	}
 
 	public double getIterativa(int n) {
 		if (n < 0) {
